3923685a9ceca706a409c61a3f7e7d7ee3953bce
Raquel Grãos Rodrigues <raquelgrodrigues@tecnico.ulisboa.pt>
2025-05-14 12:16:05 +0100

    Added trailer and support for alternating between wireframe and solid materials

--- Trab_B/js/main-script-B.js
+++ Trab_B/js/main-script-B.js
@@ -1,8 +1,8 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+/*import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { VRButton } from "three/addons/webxr/VRButton.js";
 import * as Stats from "three/addons/libs/stats.module.js";
-import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";*/
 
 //////////////////////
 /* GLOBAL VARIABLES */
@@ -10,6 +10,11 @@ import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 
 var cameras = [];
 var camera, scene, renderer;
+
+var trailer, box, append;
+var wheel;
+
+var materials = new Map();
 
 /////////////////////
 /* CREATE SCENE(S) */
@@ -20,6 +25,9 @@ function createScene() {
     scene = new THREE.Scene();
 
     scene.background = new THREE.Color('#ffffff');
+
+    //createRobot();
+    createTrailer(0, 30, 0);
 }
 
 //////////////////////
@@ -27,9 +35,9 @@ function createScene() {
 //////////////////////
 function createCameras() {
     'use strict';
-    const camera_pos = new Array(new Array(100, 0, 0), // proj. ortogonal - frontal
-                                new Array(0, 0, 100), // proj. ortogonal - lateral
-                                new Array(0, 150, 0), // proj. ortogonal - topo
+    const camera_pos = new Array(new Array(0, 0, 200), // proj. ortogonal - frontal
+                                new Array(200, 0, 0), // proj. ortogonal - lateral
+                                new Array(0, 250, 0), // proj. ortogonal - topo
                                 new Array(500, 500, 500)); // proj. perspetiva
 
     for (let i = 0; i < 4; i++) {
@@ -58,6 +66,49 @@ function createCameras() {
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+function createMaterials() {
+    'use strict';
+    materials.set("trailer", new THREE.MeshBasicMaterial({ color:0xa5a4a4, wireframe: false}));
+    materials.set("append", new THREE.MeshBasicMaterial({ color: 0x152357, wireframe: false }));
+    materials.set("wheel", new THREE.MeshBasicMaterial({ color: 0x161717, wireframe: false }));
+}
+
+function createRobot() {
+
+}
+
+function createTrailer(x, y, z) {
+    'use strict';
+
+    box = new THREE.Mesh(new THREE.BoxGeometry(100, 140, 240), materials.get("trailer")); // (0.05, 0.07. 0.12) -> escala 2000:1
+    box.position.set(0, 0, 0);
+
+    append = new THREE.Mesh(new THREE.BoxGeometry(10, 20, 10), materials.get("append")); // (0.005, 0.01, 0.005)
+    append.position.set(0, -80, 105);
+
+    trailer = new THREE.Object3D();
+    trailer.add(box);
+    trailer.add(append);
+
+    scene.add(trailer);
+
+    addWheel(trailer, 30, -85, -90); // (0.015, 0.0425, 0.045)
+    addWheel(trailer, 30, -85, -45); // (0.015, 0.0425, 0.0225)
+    addWheel(trailer, -30, -85, -90);
+    addWheel(trailer, -30, -85, -45);
+
+    trailer.position.set(x, y, z);
+}
+
+function addWheel(obj, x, y, z) {
+
+    wheel = new THREE.Mesh(new THREE.CylinderGeometry(15, 15, 20), materials.get("wheel")); // radious top: 0.0075, radius bottom: 0.0075, height: 0.01
+    wheel.rotation.z = Math.PI / 2;
+    wheel.position.set(x, y, z);
+
+    obj.add(wheel);
+
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -77,7 +128,10 @@ function update() {}
 /////////////
 /* DISPLAY */
 /////////////
-function render() {}
+function render() {
+    'use_strict';
+    renderer.render(scene, camera);
+}
 
 ////////////////////////////////
 /* INITIALIZE ANIMATION CYCLE */
@@ -89,6 +143,7 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+    createMaterials();
     createScene();
     createCameras();
 
@@ -98,7 +153,11 @@ function init() {
 /////////////////////
 /* ANIMATION CYCLE */
 /////////////////////
-function animate() {}
+function animate() {
+    render();
+    
+    requestAnimationFrame(animate);
+}
 
 ////////////////////////////
 /* RESIZE WINDOW CALLBACK */
@@ -124,6 +183,10 @@ function onKeyDown(e) {
         case 52: //4
             camera = cameras[3];
             break;
+        case 55: //7
+            materials.forEach((element) => {
+                element.wireframe = !element.wireframe;
+            })
     }
 }
 
