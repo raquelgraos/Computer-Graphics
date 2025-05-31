b5a3125fd899c4e25905589dbefea6068fce35cf
Raquel Grãos Rodrigues <raquelgrodrigues@tecnico.ulisboa.pt>
2025-05-31 17:33:09 +0100

    Created house & added ambient light

--- Trab_C/js/main-script-C.js
+++ Trab_C/js/main-script-C.js
@@ -17,9 +17,10 @@ const materials = new Map();
 
 var geometry, mesh;
 var flowerField;
-var moon, ufo;
+var moon, ufo, house;
 
-var globalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+var globalLight = new THREE.DirectionalLight(0xffffff, 1);
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
 var spotlight;
 var pointLights = [];
 
@@ -39,22 +40,22 @@ function createScene() {
     scene.background = new THREE.Color('#4a4a4a');
 
     createFlowerField(0, -20, 10);
-    
+
     createMoon(25, 40, 0);
     //populateCorkOaks();
-    //createHouse();
+    createHouse(-20, 0, 0);
     createUFO(-15, 20, -4);
 }
 
 function createFlowerField(x, y, z) {
-    
+
     geometry = new THREE.PlaneGeometry(200, 150, 100, 100);
     mesh = new THREE.Mesh(geometry, materials.get("flowerField"));
 
     flowerField = new THREE.Object3D();
     flowerField.add(mesh);
 
-    flowerField.rotation.x = 3*Math.PI / 2;
+    flowerField.rotation.x = 3 * Math.PI / 2;
     flowerField.position.set(x, y, z);
     scene.add(flowerField);
 
@@ -65,13 +66,13 @@ function createFlowerField(x, y, z) {
 //////////////////////
 
 function createCameras() {
-    
+
     camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 1000);
-    
-    camera.position.set(80, 55, 80); 
+
+    camera.position.set(80, 55, 80);
     camera.lookAt(scene.position);
 
-    cameras.push(camera);   
+    cameras.push(camera);
 }
 
 /////////////////////
@@ -81,15 +82,17 @@ function createCameras() {
 function createLights() {
 
     globalLight.position.set(moon.position.x, moon.position.y, moon.position.z);
-    globalLight.target.position.set(Math.sin(Math.PI / 4), 0, Math.cos(Math.PI / 4));
+    globalLight.target.position.set(10, 10, 10);
     scene.add(globalLight);
     scene.add(globalLight.target);
+
+    scene.add(ambientLight);
 }
 
 function updatePointlights() {
     pointLights.forEach((light) => {
-                light.visible = !light.visible;
-            })
+        light.visible = !light.visible;
+    })
 }
 
 ////////////////////////
@@ -101,21 +104,22 @@ function createMaterials() {
     const texture = loader.load('./heightmap.png');
 
     materials.set("flowerField", new THREE.MeshPhongMaterial({ bumpMap: texture, bumpScale: 5, displacementMap: texture, displacementScale: 20 }));
-    
+
     materials.set("moon", new THREE.MeshLambertMaterial({ color: 0xffffff, emissive: 0xffffff, emissiveIntensity: 0.8 })); //white
-    
+
     materials.set("stripped trunk", new THREE.MeshBasicMaterial({ color: 0xa14a0d })); // orange brown
     materials.set("trunk", new THREE.MeshBasicMaterial({ color: 0x3d1c05 })); // dark brown
     materials.set("leaves", new THREE.MeshBasicMaterial({ color: 0x1b3d05 })); // dark green
-    
+
     materials.set("ufo", new THREE.MeshLambertMaterial({ color: 0x666464 })); // dark gray
     materials.set("cockpit", new THREE.MeshLambertMaterial({ color: 0x808080 }));  // gray
     materials.set("cylinder", new THREE.MeshLambertMaterial({ color: 0xf5a958 })); // light orange
     materials.set("light", new THREE.MeshLambertMaterial({ color: 0x78f556 })); // neon green
-    
-    materials.set("walls", new THREE.MeshBasicMaterial({ color: 0xffffff })); //white
-    materials.set("door", new THREE.MeshBasicMaterial({ color: 0x358edb })); // vivid blue
-    materials.set("roof", new THREE.MeshBasicMaterial({ color: 0xde5721 })); // vivid orange
+
+    materials.set("walls", new THREE.MeshLambertMaterial({ color: 0xffffff })); //white
+    materials.set("window", new THREE.MeshLambertMaterial({ color: 0x358edb })); // vivid blue
+    materials.set("door", new THREE.MeshLambertMaterial({ color: 0x358edb })); // vivid blue
+    materials.set("roof", new THREE.MeshLambertMaterial({ color: 0xde5721 })); // vivid orange
 }
 
 function createMoon(x, y, z) {
@@ -135,7 +139,7 @@ function addLights() {
 
     let angle;
     for (let i = 0; i < nLights; i++) {
-        
+
         geometry = new THREE.SphereGeometry(0.20, 25, 50);
         mesh = new THREE.Mesh(geometry, materials.get("light"));
         mesh.position.set(2.5, -0.2, 0);
@@ -158,12 +162,12 @@ function addLights() {
 function createUFO(x, y, z) {
 
     ufo = new THREE.Object3D();
-    
+
     // body
     geometry = new THREE.SphereGeometry(3, 25, 50);
     mesh = new THREE.Mesh(geometry, materials.get("ufo"));
     mesh.scale.set(1, 0.2, 1);
-    
+
     ufo.add(mesh);
 
     // cockpit
@@ -179,7 +183,7 @@ function createUFO(x, y, z) {
     mesh.position.set(0, -0.5, 0);
 
     spotlight = new THREE.SpotLight(0xf5c958, 0.7);
-    spotlight.target.position.set(0, -20, 0); // FIX: não funciona idk
+    spotlight.target.position.set(0, -40, 0); // FIX: não funciona idk
 
     mesh.add(spotlight);
     mesh.add(spotlight.target);
@@ -238,15 +242,485 @@ function createCorkOak(x, y, z) {
     scene.add(corkOak);
 }
 
+function createHouse(x, y, z) {
+
+    house = new THREE.Object3D();
+
+    buildFrontAndBackWalls(x, y, z);
+    buildSideWalls(x - 5, y, z + 11);
+
+    buildWindowsAndDoor(x, y, z);
+
+    buildRoof(x - 5, y, z);
+
+    house.position
+    scene.add(house);
+}
+
+function buildSideWalls(x, y, z) {
+
+    // Visible
+
+    var wall = new THREE.Group();
+
+    var vertices = new Float32Array([
+        0, 0, 0,  // bottom left (0)
+        2, 0, 0,  // bottom right (1)
+        0, 5, 0,  // top left (2)
+        2, 5, 0   // top right (3)
+    ]);
+
+    var indexes = [ // anti clockwise
+        0, 1, 3,
+        0, 3, 2
+    ];
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3)); // 3 components per vertex
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    vertices = new Float32Array([
+        2, 0, 0,  // bottom left (0)
+        3, 0, 0,  // bottom right (1)
+        2, 2, 0,  // top right (2)
+        3, 2, 0   // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    vertices = new Float32Array([
+        2, 3, 0,  // bottom left (0)
+        3, 3, 0,  // bottom right (1)
+        2, 5, 0,   // top right (2)
+        3, 5, 0,  // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    vertices = new Float32Array([
+        3, 0, 0,  // bottom left (0)
+        5, 0, 0,  // bottom right (1)
+        3, 5, 0,   // top right (2)
+        5, 5, 0,  // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    wall.position.set(x, y, z);
+    house.add(wall);
+
+    // Not visible
+
+    wall = new THREE.Group();
+
+    vertices = new Float32Array([
+        0, 0, 0,
+        5, 0, 0,
+        0, 5, 0,
+        5, 5, 0
+    ])
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();    
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    wall.position.set(x, y, z - 11);
+    house.add(wall);
+}
+
+function buildFrontAndBackWalls(x, y, z) {
+
+    // Visible
+
+    var wall = new THREE.Group();
+
+    var vertices = new Float32Array([
+        0, 0, 0,  // bottom right (0)
+        0, 0, 2,  // bottom left (1)
+        0, 5, 0,  // top right (2)
+        0, 5, 2   // top left (3)
+    ]);
+
+    var indexes = [ // anti clockwise
+        0, 2, 3,
+        0, 3, 1
+    ];
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3)); // 3 components per vertex
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    vertices = new Float32Array([
+        0, 0, 2,  // bottom right (0)
+        0, 0, 3,  // bottom left (1)
+        0, 2, 2,  // top right (2)
+        0, 2, 3   // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    vertices = new Float32Array([
+        0, 3, 2,  // bottom right (0)
+        0, 3, 3,  // bottom left (1)
+        0, 5, 2,  // top right (2)
+        0, 5, 3   // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    vertices = new Float32Array([
+        0, 0, 3,  // bottom right (0)
+        0, 0, 5,  // bottom left (1)
+        0, 5, 3,  // top right (2)
+        0, 5, 5   // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    vertices = new Float32Array([
+        0, 3, 5,  // bottom right (0)
+        0, 3, 6,  // bottom left (1)
+        0, 5, 5,  // top right (2)
+        0, 5, 6   // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    vertices = new Float32Array([
+        0, 0, 6,  // bottom right (0)
+        0, 0, 8,  // bottom left (1)
+        0, 5, 6,  // top right (2)
+        0, 5, 8   // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    vertices = new Float32Array([
+        0, 0, 8,  // bottom right (0)
+        0, 0, 9,  // bottom left (1)
+        0, 2, 8,  // top right (2)
+        0, 2, 9   // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    vertices = new Float32Array([
+        0, 3, 8,  // bottom right (0)
+        0, 3, 9,  // bottom left (1)
+        0, 5, 8,  // top right (2)
+        0, 5, 9   // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    vertices = new Float32Array([
+        0, 0, 9,  // bottom right (0)
+        0, 0, 11,  // bottom left (1)
+        0, 5, 9,  // top right (2)
+        0, 5, 11   // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    wall.position.set(x, y, z);
+    house.add(wall);
+
+    // Not visible
+
+    wall = new THREE.Group();
+
+    vertices = new Float32Array([
+        0, 0, 0,
+        0, 0, 11,
+        0, 5, 0,
+        0, 5, 11
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("walls"));
+    wall.add(mesh);
+
+    wall.position.set(x - 5, y, z);
+    house.add(wall);
+}
+
+function buildWindowsAndDoor(x, y, z) {
+
+    // Side window
+
+    var window = new THREE.Group();
+
+    var vertices = new Float32Array([
+        2, 2, 0, // bottom left (0)
+        3, 2, 0, // bottom right (1)
+        2, 3, 0, // top left (2)
+        3, 3, 0  // top right (3)
+    ]);
+
+    var indexes = [ // anti clockwise
+        0, 1, 3,
+        0, 3, 2
+    ];
+    
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3)); // 3 components per vertex
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("window"));
+    window.add(mesh);
+
+    window.position.set(x - 5, y, z + 11);
+    house.add(window);
+
+    // Front windows
+
+    window = new THREE.Group();
+
+    vertices = new Float32Array([
+        0, 2, 2, // bottom right (0)
+        0, 2, 3, // bottom left (1)
+        0, 3, 2, // top right (2)
+        0, 3, 3  // top left (3)
+    ]);
+
+    indexes = [ // anti clockwise
+        0, 2, 3,
+        0, 3, 1
+    ];
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("window"));
+    window.add(mesh);
+
+    window.position.set(x, y, z);
+
+    vertices = new Float32Array([
+        0, 2, 8, // bottom right (0)
+        0, 2, 9, // bottom left (1)
+        0, 3, 8, // top right (2)
+        0, 3, 9  // top left (3)
+    ]);
+
+    indexes = [
+        0, 3, 1,
+        0, 2, 3
+    ];
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("window"));
+    window.add(mesh);
+
+    window.position.set(x, y, z);
+
+    house.add(window);
+
+    // Front door
+
+    var door = new THREE.Group();
+
+    vertices = new Float32Array([
+        0, 0, 5, // bottom right (0)
+        0, 0, 6, // bottom left (1)
+        0, 3, 5, // top right (2)
+        0, 3, 6  // top left (3)
+    ]);
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("door"));
+    door.add(mesh);
+
+    door.position.set(x, y, z);
+
+    house.add(door);    
+
+}
+
+function buildRoof(x, y, z) {
+
+    var roof = new THREE.Group();
+
+    // Not visible 
+    var vertices = new Float32Array([
+        0, 5, 0,    // bottom left (0)
+        0, 5, 11,   // bottom right (1)
+        2.5, 7, 0,  // top left (2)
+        2.5, 7, 11  // top right (3)
+    ]);
+
+    var indexes = [ // anti clockwise
+        0, 1, 3,
+        0, 3, 2
+    ];
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3)); // 3 components per vertex
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("roof"));
+    roof.add(mesh);
+
+    // Visible
+
+    vertices = new Float32Array([
+        2.5, 7, 0,  // bottom left (0)
+        2.5, 7, 11, // bottom right (1)
+        5, 5, 0,    // top left (2)
+        5, 5, 11    // top right (3)
+    ]); 
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("roof"));
+    roof.add(mesh);
+
+    // Not visible
+
+    vertices = new Float32Array([
+        0, 5, 0,   // bottom left (0)
+        2.5, 7, 0, // top (1)
+        5, 5, 0    // bottom right (2)
+    ]);  
+
+    indexes = [ // anti clockwise
+        0, 2, 1
+    ];
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("roof"));
+    roof.add(mesh);
+
+    // Visible
+
+    vertices = new Float32Array([
+        0, 5, 11,   // bottom left (0)
+        2.5, 7, 11, // top (1)
+        5, 5, 11    // bottom right (2)
+    ]);  
+
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indexes);
+    geometry.computeVertexNormals();
+
+    mesh = new THREE.Mesh(geometry, materials.get("roof"));
+    roof.add(mesh);
+
+    roof.position.set(x, y, z);
+    house.add(roof);
+}
+
 //////////////////////
 /* CHECK COLLISIONS */
 //////////////////////
-function checkCollisions() {}
+function checkCollisions() { }
 
 ///////////////////////
 /* HANDLE COLLISIONS */
 ///////////////////////
-function handleCollisions() {}
+function handleCollisions() { }
 
 ////////////
 /* UPDATE */
@@ -267,7 +741,7 @@ function handleUFOMovement() {
     ufo.rotation.y += 0.008; // ufo spins in constant speed
 
     movementVector.set(0, 0, 0);
-    
+
     if (leftKey) {
         movementVector.z += 10;
     } if (upKey) {
@@ -300,10 +774,10 @@ function render() {
 function init() {
 
     renderer = new THREE.WebGLRenderer({
-            antialias: true,
-        });
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+        antialias: true,
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
     createMaterials();
     createScene();
@@ -323,7 +797,7 @@ function animate() {
     update();
 
     render();
-    
+
     requestAnimationFrame(animate);
 }
 
@@ -346,7 +820,7 @@ function onResize() {
 ///////////////////////
 function onKeyDown(e) {
 
-    switch(e.keyCode) {
+    switch (e.keyCode) {
         case 37: // left arrow
             leftKey = true;
             break;
@@ -371,9 +845,9 @@ function onKeyDown(e) {
             updatePointlights();
             break;
         case 81: // q
-           break;
+            break;
         case 82: // r
-           break;
+            break;
         case 83: // s
             spotlight.visible = !spotlight.visible;
             break;
